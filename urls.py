ae7c47152d5fbd4165573eb913cac2836f3015b5
Stevee67 <stopa6767@gmail.com>
2016-05-30 17:45:59 +0300

    m

--- urls.py
+++ urls.py
@@ -2,11 +2,4 @@ from handlers import HomeHandler
 
 hundlers = [
             (r"/", HomeHandler),
-            # (r"/archive", ArchiveHandler),
-            # (r"/feed", FeedHandler),
-            # (r"/entry/([^/]+)", EntryHandler),
-            # (r"/compose", ComposeHandler),
-            # (r"/auth/create", AuthCreateHandler),
-            # (r"/auth/login", AuthLoginHandler),
-            # (r"/auth/logout", AuthLogoutHandler),
         ]
